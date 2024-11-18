ec5a321ca592974c6cd70a0b0585370c8ea45dff
Lex Garey-Fernandez <lexgarey@gmail.com>
2024-11-17 20:59:14 -0700

    fix mongodb issues

--- connect.js
+++ connect.js
@@ -1,16 +1,26 @@
-require('dotenv').config()
-const { createClient } = require('@supabase/supabase-js')
 
-const supabaseUrl = 'https://cmttuqgmhgsxbgrpblkf.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://lexgarey:E1OwResn7HD6Cj7M@cluster0.unflyuk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-const getTarotCards = async () => {
-    const { data, error } = await supabase
-      .from('tarot_cards')
-      .select()
-    
-      console.log(data)
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 }
-
-getTarotCards()
+run().catch(console.dir);
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const { createClient } = require('@supabase/supabase-js')
 
-const supabaseUrl = 'https://cmttuqgmhgsxbgrpblkf.supabase.co'
+const supabaseUrl = 'https://cmttuqgmhgsxbgrpblkf.supabase.co/'
 const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
@@ -11,78 +11,30 @@ const PORT = 3000
 
 const getTarotCards = async () => {
     
-    let { data: tarot_cards, error } = await supabase
+    const { data: tarot_cards, error } = await supabase
     .from('tarot_cards')
     .select('*')
 
     if (error) {
-        console.log(error)
+        console.log('Error fetching tarot cards', error)
         return []
     }
-
     return tarot_cards
     }
 
 const tarotCards = getTarotCards()
 
-const fetchTarotCards = async (req, res, next) => {
-    const query = req.query
-    req.tarotCards = await getTarotCards(query)
-    next()
-}
-
-// app.use('/cards', fetchTarotCards)
+// app.use('/', fetchTarotCards)
     
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-
 app.get('/cards', async (req, res) => {
-    const {data, error} = await supabase
-        .from('tarot_cards')
-        .select()
-    res.send(data)
+    const tarotCards = await tarotCards()
+    res.json(tarotCards)
 })
-
-app.get('/cards/:name', async (req, res) => {
-    const {data, error} = await supabase
-        .from('tarot_cards')
-        .select()
-        .is('name', req.params.name)
-    res.send(data)
-})
-
-app.get('/cards/:id', async (req, res) => {
-    const {data, error} = await supabase
-        .from('tarot_cards')
-        .select()
-        .is('id', req.params.id)
-    res.send(data)
-})  
-
-
-// // get all cards
-// app.get('v1/cards', async (req, res) => {
-//     console.log(tarotCards)  
-//     res.json(tarotCards)    
-// })
-
-// get card by name
-// app.get('/cards/:name', async (req, res) => {
-//     const cardName = req.params.name.toLowerCase()
-//     const card = tarotCards.find(card => card.name === cardName)
-//     res.json(card)
-// })  
-
-// // get cards by arcana
-// app.get('/cards/arcana/:arcana', async (req, res) => {
-//     const arcana = req.params.arcana.toLowerCase()
-//     const cards = tarotCards.filter(card => card.arcana === arcana)
-//     res.json(cards)
-// })
-
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+    console.log(`Server is running on http://localhost:${PORT}`)
 })
